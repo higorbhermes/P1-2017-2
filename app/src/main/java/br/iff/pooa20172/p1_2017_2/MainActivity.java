b2649d4132e46f5fa6980f95e28bdecbe9deb123
higorbhermes <higorbhermes@gmail.com>
2017-12-15 11:55:56 -0200

    ActivityQuemSomos e Funcionamento dos Botões QuemSomos e Sair

--- app/src/main/java/br/iff/pooa20172/p1_2017_2/MainActivity.java
+++ app/src/main/java/br/iff/pooa20172/p1_2017_2/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button_Pedido = (Button) findViewById(R.id.button_Pedido);
+        Button button_QuemSomos = (Button) findViewById(R.id.button_QuemSomos);
+        Button button_Sair = (Button) findViewById(R.id.button_Sair);
         button_Pedido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -20,6 +22,20 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+        button_QuemSomos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ActivityQuemSomos.class);
+                startActivity(intent);
+            }
+        });
+        button_Sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
+
+
     }
 }
